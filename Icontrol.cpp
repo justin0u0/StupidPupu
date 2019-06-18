941349773c8999fb66ccfb7297f003c6d6907ae0
justin <dancinglinkxalgorithm@gmail.com>
2019-06-18 14:32:44 +0800

    Chore(IControl):

---
+++ Icontrol.cpp
@@ -0,0 +1,8 @@
+#include "IControl.hpp"
+
+void IControl::OnKeyDown(int keyCode) {}
+void IControl::OnKeyUp(int keyCode) {}
+void IControl::OnMouseDown(int button, int mx, int my) {}
+void IControl::OnMouseUp(int button, int mx, int my) {}
+void IControl::OnMouseMove(int mx, int my) {}
+
