1c6eae0eabba7f838c77b0c61830001e263faf22
justin0u0 <38185674+justin0u0@users.noreply.github.com>
2019-06-20 13:33:42 +0800

    Feat(IScene): add Loader to IScene

--- IScene.cpp
+++ IScene.cpp
@@ -1,6 +1,9 @@
 #include <allegro5/allegro.h>
 #include "IScene.hpp"
 
+IScene::IScene() {
+	loader = new Loader();
+}
 void IScene::Draw() const {
 	al_clear_to_color(al_map_rgb(0, 0, 0));
 }
--- IScene.hpp
+++ IScene.hpp
@@ -1,10 +1,12 @@
 #ifndef ISCENE_HPP
 #define ISCENE_HPP
 
+#include "Loader.hpp"
+
 class IScene {
 protected:
 	// IScene can only not instantiated directed
-	explicit IScene() = default;
+	explicit IScene();
 public:
 	// Copy constructor is deleted
 	IScene(IScene const&) = delete;
@@ -12,6 +14,7 @@ public:
 	IScene& operator=(IScene const&) = delete;
 	// The default virtual destructor to support polymorphism destruction
 	virtual ~IScene() = default;
+	Loader *loader;
 	virtual void Initialize() = 0;
 	virtual void Terminate() = 0;
 	void Draw() const;
