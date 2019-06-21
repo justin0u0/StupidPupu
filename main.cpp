a86438dbd0c38482a061db8260cef22d8c5b804e
Bronwin Chen <sk99g30622@gmail.com>
2019-06-21 23:54:34 +0800

    Fix(main): Change initial scene to start scene

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include "GameEngine.hpp"
 #include "SelectScene.hpp"
 #include "PlayScene.hpp"
+#include "StartScene.hpp"
 
 int main() {
 	GameEngine &game = GameEngine::GetInstance();
-	game.AddNewScene("start", nullptr);
+	game.AddNewScene("start", new StartScene());
 	game.AddNewScene("select", new SelectScene());
 	game.AddNewScene("play", new PlayScene());
-	game.Start(60, 1536, 960, 15, "StupidPupu", "select");
+	game.Start(60, 1536, 960, 15, "StupidPupu", "start");
 	return 0;
 }
 
