4f66af32a126d6b6ba35669472a4f4abbb3badd5
Bronwin Chen <sk99g30622@gmail.com>
2019-06-22 02:12:06 +0800

    Feat(SelectScene): draw text on image_button when mouse in

--- SelectScene.cpp
+++ SelectScene.cpp
@@ -9,6 +9,7 @@ void SelectScene::Initialize() {
 	for (int i = 0; i < 3; i++) {
 		saves[i] = new ImageButton("square_button_light_gray.png", "square_button_gray.png", dx * (i * 2 + 1), 400, 400, 400, 0.5, 0);
 		saves[i]->SetOnClick(std::bind(&SelectScene::Start, this));
+		start[i] = new Text("START", "Metrool.ttf", 128, dx * (i * 2 + 1), 550, 100, 150, 200, 255, 0.5, 0);
 	}
 	bgm = Music::PlayBGM("LA.wav");
 }
@@ -22,12 +23,16 @@ void SelectScene::OnMouseDown(int button, int mx, int my) {
 void SelectScene::OnMouseMove(int mx, int my) {
 	for (int i = 0; i < 3; i++)
 		saves[i]->OnMouseMove(mx, my);
+	
 }
 void SelectScene::Draw() const {
 	IScene::Draw();
 	background->Draw();
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < 3; i++) {
 		saves[i]->Draw();
+		if (saves[i]->IsMouseIn())
+			start[i]->Draw();
+	}
 }
 void SelectScene::Start() {
 	GameEngine::GetInstance().SetNextScene("play");
