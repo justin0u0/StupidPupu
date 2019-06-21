f977f5100b7ba32b91051851f507981dbddc080b
justin0u0 <38185674+justin0u0@users.noreply.github.com>
2019-06-21 08:02:41 +0800

    Feat(main.cpp): pass reserve samples

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main() {
 	game.AddNewScene("start", nullptr);
 	game.AddNewScene("select", new SelectScene());
 	game.AddNewScene("play", new PlayScene());
-	game.Start(60, 1536, 960, "StupidPupu", "select");
+	game.Start(60, 1536, 960, 15, "StupidPupu", "select");
 	return 0;
 }
 
