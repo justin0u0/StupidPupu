ee0630b0725dd28fd3d0e563e33fb051a55334ec
justin0u0 <38185674+justin0u0@users.noreply.github.com>
2019-06-21 11:25:50 +0800

    Style(IControl): naming keyCode->keycode

--- IControl.cpp
+++ IControl.cpp
@@ -1,7 +1,7 @@
 #include "IControl.hpp"
 
-void IControl::OnKeyDown(int keyCode) {}
-void IControl::OnKeyUp(int keyCode) {}
+void IControl::OnKeyDown(int keycode) {}
+void IControl::OnKeyUp(int keycode) {}
 void IControl::OnMouseDown(int button, int mx, int my) {}
 void IControl::OnMouseUp(int button, int mx, int my) {}
 void IControl::OnMouseMove(int mx, int my) {}
--- IControl.hpp
+++ IControl.hpp
@@ -10,8 +10,8 @@ class IControl {
 		IContol(const IControl& other);
 		IControl& operator=(IControl const&) = default;
 		
-		virtual void OnKeyDown(int keyCode);
-		virtual void OnKeyUp(int keyCode);
+		virtual void OnKeyDown(int keycode);
+		virtual void OnKeyUp(int keycode);
 		virtual void OnMouseDown(int button, int mx, int my);
 		virtual void OnMouseUp(int button, int mx, int my);
 		virtual void OnMouseMove(int mx, int my);
