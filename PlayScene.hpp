b210d107dbb065a98cd7b31a9a6c8a0dc5133849
Bronwin Chen <sk99g30622@gmail.com>
2019-06-22 22:48:08 +0800

    Feat(PlayScene): add bgm for PlayScene

--- PlayScene.hpp
+++ PlayScene.hpp
@@ -1,20 +1,23 @@
 #ifndef PLAYSCENE_HPP
 #define PLAYSCENE_HPP
-
+#include <allegro5/allegro_audio.h>
 #include <vector>
 #include "IScene.hpp"
 #include "Land.hpp"
 #include "Player.hpp"
+#include "Setting.hpp"
 
 class PlayScene final : public IScene {
 private:
 	std::vector<Land *> lands;
 	Player* player;
+	ALLEGRO_SAMPLE_INSTANCE *bgm_instance, *sfx_instance;
 public:
 	void Initialize() override;
 	void Terminate() override;
 	void Draw() const override;
 	void Update(float deltaTime) override;
+	void OnKeyDown(int keycode) override;
 };
 #endif
 
