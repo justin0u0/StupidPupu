a87077a83aa6329754e19699bd0699feb85d88c2
justin0u0 <38185674+justin0u0@users.noreply.github.com>
2019-06-23 01:20:40 +0800

    Feat(Land): new function spawn and others function spawn needed

--- Land.hpp
+++ Land.hpp
@@ -1,17 +1,30 @@
 #ifndef LAND_HPP
 #define LAND_HPP
 
+#include <vector>
 #include "Sprite.hpp"
+#include "ResourceInfo.hpp"
+#include "Resource.hpp"
+#include "Point.hpp"
 
 class Land : public Sprite {
 private:
+	const int Size = 16;
+	const int Capacity = Size * Size / 2;
+	float spawn_cooldown = 0.0;
+	std::vector<std::vector<bool>> land_state;
 	// TODO: belong's enemies
 	// TODO: unlock requirement;
-	bool locked = true;
+	// TODO: locked
+	std::vector<ResourceInfo> resource_types;
+	std::vector<Resource *> resources;
+	std::vector<int> GetResourceWeights();
+	Point NewSpawnPoint();
 public:
 	explicit Land(std::string img, int x, int y);
-//	void Unlock();
 	void Update(float deltaTime) override;
+	void AddNewResourceType(std::string name);
+	void Spawn(float deltaTime);
 };
 #endif
 
