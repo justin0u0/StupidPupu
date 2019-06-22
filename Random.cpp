5a1924aaa3aad6c17cd725229307461e12c69131
justin0u0 <38185674+justin0u0@users.noreply.github.com>
2019-06-23 02:48:40 +0800

    Fix(Random): add seed to Random and rebuild function

--- Random.cpp
+++ Random.cpp
@@ -1,26 +1,23 @@
+#include <random>
 #include "Random.hpp"
 
-#include <random>
-
-int Random::RandomInteger(int min_val, int max_val) {
-	static std::random_device rd;
-	static std::default_random_engine gen(rd());
+int Random::Integer(int min_val, int max_val) {
 	std::uniform_int_distribution<int> dis(min_val, max_val);
 	return dis(gen);
 }
-float Random::RandomFloat(float min_val, float max_val) {
-	static std::random_device rd;
-	static std::default_random_engine gen(rd());
+float Random::Float(float min_val, float max_val) {
 	std::uniform_real_distribution<float> dis(min_val, max_val);
 	return dis(gen);
 }
-std::vector<int> Random::RandomWeightedIntergerSequence(int size, std::vector<int> weights) {
-	static std::random_device rd;
-	static std::default_random_engine gen(rd());
+std::vector<int> Random::WeightedIntergerSequence(int size, std::vector<int> weights) {
 	std::discrete_distribution<int> dis(weights.begin(), weights.end());
 	std::vector<int> result;
 	for (int i = 0; i < size; i++)
 		result.emplace_back(dis(gen));
 	return result;
+}
+Random& Random::GetRandom() {
+	static Random random;
+	return random;
 }
 
--- Random.hpp
+++ Random.hpp
@@ -3,13 +3,17 @@
 
 #include <random>
 #include <vector>
+#include <chrono>
 
 class Random {
+private:
+	std::default_random_engine gen = std::default_random_engine(static_cast<long unsigned int>(std::chrono::high_resolution_clock::now().time_since_epoch().count()));
+	explicit Random() = default;
 public:
-	explicit Random() = delete;
-	static int RandomInteger(int min_val, int max_val);
-	static float RandomFloat(float min_val, float max_val);
-	static std::vector<int> RandomWeightedIntergerSequence(int size, std::vector<int> weights);
+	int Integer(int min_val, int max_val);
+	float Float(float min_val, float max_val);
+	std::vector<int> WeightedIntergerSequence(int size, std::vector<int> weights);
+	static Random& GetRandom();
 };
 #endif
 
