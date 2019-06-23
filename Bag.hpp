4e78b586793d4fcc71260488801a19a60e8614ea
Bronwin Chen <sk99g30622@gmail.com>
2019-06-23 19:44:53 +0800

    Chore(Bag): add struct BagCell

--- Bag.hpp
+++ Bag.hpp
@@ -3,20 +3,23 @@
 #include "Image.hpp"
 #include "ImageButton.hpp"
 #include "Text.hpp"
+#include <unordered_map>
+
+struct BagCell {
+	Image* img;
+	ImageButton *button;
+	Text *amount, *name;
+};
 
 class Bag : public Image , public IControl{
 private:
 	static const int Size = 20;
-	static const int MixSize = 4;
-//	Image *item[20];
-	ImageButton *square[Size];
-//	Text *info[20];
-	// for mix function
-//	Image *mix_item[4];
-	ImageButton *mix_square[4];
-//	Text *mix_info[4];
+	static const int MixSize = 3;
   	ImageButton *mix_button;
+  	std::unordered_map<std::string, int> package; // (item name, total amount)
 public:
+	BagCell bag_cell[Size];
+	BagCell mix_cell[MixSize];
 	explicit Bag();
 	void Draw() const override;
 	void OnMouseDown(int button, int mx, int my) override;
---
+++ BagCell.hpp
@@ -0,0 +1,12 @@
+#include "Image.hpp"
+#include "ImageButton.hpp"
+#include "Text.hpp"
+
+// a cell in bag
+struct BagCell {
+	Image* img;
+	ImageButton *button;
+	Text *amount *name;
+	int amount = 0;
+};
+
