4ef79dbd8a0e1be398ddd8c93d3f535ad82ec967
justin0u0 <38185674+justin0u0@users.noreply.github.com>
2019-06-23 13:47:40 +0800

    Chore(EnemyInfo): Create Class EnemyInfo

---
+++ EnemyInfo.hpp
@@ -0,0 +1,20 @@
+#ifndef ENEMYINFO_HPP
+#define ENEMYINTO_HPP
+
+#include <string>
+
+struct EnemyInfo {
+	std::string type;
+	std::string img;
+	int maximum_hp;
+	int damage;
+	float speed;
+	float detect_radius;
+	float attack_cooldown;
+	explicit EnemyInfo(std::string type, std::string img, int maximum_hp
+		, int damage, float speed, float detect_radius, float attack_cooldown, int universality);
+	EnemyInfo(const EnemyInfo&) = default;
+	EnemyInfo& operator=(const EnemyInfo&) = default;
+};
+#endif
+
