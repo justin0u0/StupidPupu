1b8d4674277c041e376b7565ab0189951ccd5a8f
justin0u0 <38185674+justin0u0@users.noreply.github.com>
2019-06-23 15:11:59 +0800

    Feat(Enemy): move randomly

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,5 +1,7 @@
+#include <allegro5/allegro.h>
 #include "Enemy.hpp"
 #include "Random.hpp"
+#include "GameEngine.hpp"
 
 Enemy::Enemy(std::string img, float x, float y, float w, float h
 	, int health, int damage, float speed, float detect_radius, float attack_cooldown)
@@ -9,12 +11,24 @@ Enemy::Enemy(std::string img, float x, float y, float w, float h
 void Enemy::Update(float deltaTime) {
 	change_direction_cooldown -= deltaTime;
 	if (change_direction_cooldown < 0) {
-		change_direction_cooldown = Random::GetRandom().Float(5, 20);
+		change_direction_cooldown = Random::GetRandom().Float(3, 5);
 		velocity.x = Random::GetRandom().Float(-1, 1);
 		velocity.y = sqrt(1 - velocity.x * velocity.x);
 		if (Random::GetRandom().Integer(0, 1))
 			velocity.y *= -1;
 		velocity *= speed;
 	}
+//		velocity.x = -60;
+//	else if (game.IsRight())
+//		velocity.x += 60;
+//	if (game.IsUp())
+//		velocity.y -= 60;
+//	else if (game.IsDown())
+//		velocity.y += 60;
+	if (velocity.x > 0)
+		flag = ALLEGRO_FLIP_HORIZONTAL;
+	else
+		flag = 0;
+	Sprite::Update(deltaTime);
 }
 
