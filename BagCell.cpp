ca6f6bd0efcf70c34ea69bded778ea22d7dbb891
Bronwin Chen <sk99g30622@gmail.com>
2019-06-24 10:23:30 +0800

    Chore(BagCell): as a cell in bag

--- BagCell.cpp
+++ BagCell.cpp
@@ -1,1 +1,46 @@
+#include "BagCell.hpp"
+#include "Log.hpp"
+#include "PlayScene.hpp"
+#include "GameEngine.hpp" 
+
+BagCell::BagCell(int id, float x, float y) {
+	button = new ImageButton("square_out.png", "square_in.png", x, y, 130, 130);
+	img = new Image("square_out.png", x, y, 130, 130, 0, 0);
+	name = new Text("", "pirulen.ttf", 10, x + 65, y + 150, 255, 255, 255, 255, 0.5, 0.5);
+	amount = new Text("", "pirulen.ttf", 10, x + 130, y + 100, 255, 255, 255, 255, 0, 0);
+	img_name = "square_out.png";
+	is_empty = true;
+	this->id = id;
+	button->SetOnClick(std::bind(&BagCell::ButtonCallback, this));
+}
+void BagCell::ButtonCallback() {
+	PlayScene* playscene = dynamic_cast<PlayScene *>(GameEngine::GetInstance().GetActiveScene());
+	Log(Debug) << this->id;
+	playscene->bag->MixUpdate(id);
+}
+void BagCell::SetCallback(std::function<void(int id)> f) {
+	callback = f;
+}
+void BagCell::Draw(bool able) const {
+	button->Draw();
+	if (able) {
+		img->Draw();
+		name->Draw();
+		amount->Draw();
+	}
+}
+void BagCell::Update(std::string img, std::string text, int num) {
+	this->img->ChangeImage(img);
+	name->ChangeText(text);
+	amount->ChangeText(std::to_string(num));
+	img_name = img;
+}
+void BagCell::ChangeItem(BagCell b) {
+	Log(Debug) << "Bag Cell " << this->id << " changeItem to id " << b.id;
+	this->img->ChangeImage(b.img_name);
+	this->name->ChangeText(b.name->text);
+	this->amount->ChangeText(b.amount->text);
+	this->img_name = b.img_name;
+	this->is_empty = false;
+}
 
--- BagCell.hpp
+++ BagCell.hpp
@@ -1,12 +1,29 @@
+#ifndef BAGCELL_HPP
+#define BAGCELL_HPP
+
+#include <functional>
 #include "Image.hpp"
 #include "ImageButton.hpp"
 #include "Text.hpp"
 
 // a cell in bag
-struct BagCell {
+class BagCell {
+private:
+	std::function<void(int id)> callback;
+public:
 	Image* img;
 	ImageButton *button;
-	Text *amount *name;
-	int amount = 0;
-};
+	Text *amount, *name;
+	std::string img_name;
+	bool is_empty;
+	int id;
+	explicit BagCell(int id, float x, float y);
+	void SetCallback(std::function<void(int id)>);
+	void ButtonCallback();
+	void Draw(bool able) const;
+	void Update(std::string img, std::string text, int num);
+	void ChangeItem(BagCell b);
+//	explicit BagCell(int id, float x, float y);
+}; 
+#endif
 
